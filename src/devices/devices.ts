1cf76076a77346bbb28d1a7e8496337523325892
Mammad <67197540+Mammad900@users.noreply.github.com>
2023-06-29 11:35:43 +0330

    Fix crash when viewing an empty room's devices

--- src/devices/devices.ts
+++ src/devices/devices.ts
@@ -21,10 +21,10 @@ export const liveSliderStreams: Record<number, { device: DeviceInstance, interac
 
 export function getDevices(roomId: string): Record<string, HMApi.T.Device> | undefined {
     if(getRoom(roomId)) { // Check if room exists
-        return Object.fromEntries(Object.entries(devices[roomId]).map(([k, v]) => {
+        return Object.fromEntries(Object.entries(devices[roomId] || {}).map(([k, v]) => {
             const { '@extra': _, ...params } = v.params;
             return [k, { ...v, params }] as const;
-        })) || {};
+        }));
     }
 }
 
